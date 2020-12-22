156021bc718b527fa25769ccb4d61bb7e187a25c
Matias <matias.sanchez@alumnos.frgp.utn.edu.ar>
2020-12-22 18:09:29 -0300

    Modificacion lista

--- php/Busqueda.php
+++ php/Busqueda.php
@@ -33,6 +33,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="Eliminar.php">Eliminar</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="Modificar.php">Modificar</a>
+                </li>
             </ul>
         </div>
     </div>
--- php/Modificar.php
+++ php/Modificar.php
@@ -31,10 +31,10 @@
                     <a class="nav-link" href="Busqueda.php">Busqueda</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" href="Eliminar.php">Eliminar</a>
+                    <a class="nav-link" href="Eliminar.php">Eliminar</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="Modificar.php">Modificar</a>
+                    <a class="nav-link active" href="Modificar.php">Modificar</a>
                 </li>
             </ul>
         </div>
@@ -42,19 +42,24 @@
 
     <div class="container-fluid">
         <div class="container py-5">
-            <form name="inicio" action="Eliminar.php" method="post">
+            <form name="inicio" action="Modificar.php" method="post">
                 <div class="form-group">
-                    <label>Codigo del Producto a eliminar:</label>
-                    <input type="text" name="Eliminar" class="form-control">
+                    <label>Codigo del Producto a Modificar:</label>
+                    <input type="text" name="Codigo" class="form-control">
                 </div>
-                <button type="submit" name="Enviar" value="Enviar" class="btn btn-primary">Eliminar</button>
+                <div class="form-group">
+                    <label>Precio:</label>
+                    <input type="text" name="Precio" class="form-control">
+                </div>
+                <button type="submit" name="Enviar" value="Enviar" class="btn btn-primary">Modificar</button>
             </form>
         </div>
     </div>
 
     <?php
-        if(isset($_POST['Eliminar'])){
-            $Eliminar=$_POST['Eliminar'];
+        if(isset($_POST['Codigo'])){
+            $Codigo=$_POST['Codigo'];
+            $Precio=$_POST['Precio'];
             $conexion= mysqli_connect ("localhost", "root", "");
             if(mysqli_connect_errno()){ ///verificamos la coneccion
                 echo "salio mal";
@@ -64,18 +69,18 @@
             mysqli_set_charset($conexion, "utf8"); /// ponemos los caracteres en español
 
 
-            $consulta="DELETE FROM `productos` WHERE Codigo='$Eliminar'";
+            $consulta="UPDATE `productos` SET Precio='$Precio' WHERE Codigo='$Codigo'";
 
             $resultados= mysqli_query ($conexion, $consulta);
 
             if($resultados && mysqli_affected_rows($conexion)==1){
-                echo "Producto eliminado";   
+                echo "Producto Modificado";   
             }
             else{
                 if(mysqli_affected_rows($conexion)==0){///funcion de actividades en la base de datos-- en este caso 0 delete(eliminaciones)
                     echo "Producto no existente";
                 }else{
-                    echo "Error Producto no eliminado";
+                    echo "Error Producto no Modificado";
                 }
             }
 
