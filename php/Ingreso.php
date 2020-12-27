b81025c6427a1eb57c53b6d1502bcce92a828429
Matias <matias.sanchez@alumnos.frgp.utn.edu.ar>
2020-12-27 15:34:51 -0300

    Mejorando

--- php/Ingreso.php
+++ php/Ingreso.php
@@ -1,13 +1,12 @@
 <?php
 
     require("conexion.php");/// requerida para usar $conexion
-
         /// traemos todos los campos
-        $Seccion=ucfirst ($Seccion=$_POST["Seccion"]);
-        $Nombre=ucfirst ($Nombre=$_POST["Nombre"]);
+        $Seccion=$_POST["Seccion"];
+        $Nombre=$_POST["Nombre"];
         $Precio=$_POST["Precio"];
         $Fecha=$_POST["Fecha"];
-        $PaisOrigen= ucfirst ($PaisOrigen=$_POST["PaisOrigen"]);
+        $PaisOrigen= $_POST["PaisOrigen"];
         $Estado=true;
 
         if(!strcasecmp($_POST["PaisOrigen"], "Argentina")){
@@ -16,17 +15,18 @@
         
         ///base de datos
                 
-        $sql="INSERT INTO productos(Seccion, Nombre, Precio, Fecha, Importado, PaisOrigen) VALUES (?,?,?,?,?,?)";
-        
-        $prepare=mysqli_prepare($conexion, $sql);
-        
-        if(mysqli_stmt_bind_param($prepare, "ssdsss", $Seccion, $Nombre, $Precio, $Fecha, $Estado, $PaisOrigen)==false){
-                echo "error de datos";
-        }else{
-            echo ("Error Mensaje no Enviado  ");
-        }
-        echo ("  <a href='../html/index.html'>Ingreso</a>");
-        mysqli_stmt_close($prepare);//cerramos la conexion
+        $sql="INSERT INTO `productos`(`Seccion`, `Nombre`, `Precio`, `Fecha`, `Importado`, `PaisOrigen`) VALUES (?,?,?,?,?,?)";
 
+        $Prepare=mysqli_prepare($conexion, $sql);
+
+        mysqli_stmt_bind_param($Prepare,"ssssss", $Seccion, $Nombre, $Precio, $Fecha, $Estado, $PaisOrigen);
+        
+        if(mysqli_stmt_execute($Prepare)){
+            echo "Enviado";
+        }else{
+            echo "no enviado";
+        }
+        echo "  <a href='../html/index.html'>Ingreso</a>";
+        mysqli_stmt_close($Prepare);//cerramos la conexion
 
 ?>
